725c06eeeaacedf729276ca18dcce5e152119c46
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-03-26 11:11:47 +0100

    ajout dans la base de donnee

---
+++ sql/php-pdo/edit.php
@@ -0,0 +1,61 @@
+<?php 
+/* Connect to a MySQL database using driver invocation */
+$dsn = 'mysql:host=localhost;dbname=carnetadresse;charset=UTF8;';
+$user = 'root';
+$password = 'root27';
+
+$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+]);
+
+$test =  "Charleroi";
+$error = null; // gere le message d erreur
+$success = null;
+try{
+    if(isset($_POST['ville']) && isset($_POST['haut']) && isset($_POST['bas'])){
+        $query= $pdo->prepare('UPDATE météo SET ville = :ville, haut = :haut, bas = :bas WHERE ville = :ville');
+        $query->execute([
+            'ville' => $_POST['ville'],
+            'haut' => $_POST['haut'],
+            'bas' => $_POST['bas']
+        ]);
+        $success = "La ville a bien été changé";
+    }
+    $query = $pdo->prepare('SELECT * FROM météo  where ville = :id');
+    $query->execute([
+        'id' => $_GET['ville']
+    ]);
+    // prepare et execute plus safe
+    $ville = $query->fetch();
+} catch(PDOException $e){
+    $error = $e->getMessage();
+}
+?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<div class="container my-5">
+    <p>
+        <a href="./index.php">revenir a la liste</a>
+    </p>
+    <h2>Motifiez la ville ou le temps</h2>
+    <?php if ($success): ?>
+        <p class="alert alert-success"><?= $success ?></p>
+    <?php endif ?>
+    <?php if ($error): ?>
+        <p><?= $error ?></p> 
+        <!-- pas le mieux mais bien pour le debug -->
+    <?php else: ?>
+        <form action="" method="post">
+            <div class="form-group">
+                <input type="text" class="from-control" name="ville" value="<?= htmlentities($ville->ville) ?>">
+            </div>
+            <div class="form-group">
+                <input type="text" class="from-control" name="haut" value="<?= htmlentities($ville->haut) ?>">
+            </div>
+            <div class="form-group">
+                <input type="text" class="from-control" name="bas" value="<?= htmlentities($ville->bas) ?>">
+            </div>
+            <button class="btn btn-primary">Send</button>
+        </form>
+
+    <?php endif ?>
+</div>
--- sql/php-pdo/index.php
+++ sql/php-pdo/index.php
@@ -4,42 +4,92 @@ $dsn = 'mysql:host=localhost;dbname=carnetadresse;charset=UTF8;';
 $user = 'root';
 $password = 'root27';
 
-$pdo = new PDO($dsn, $user, $password);
+$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+]);
+
 /**
  *  prepare = requete prepare 
  * 
  *  exect permet d exececuter une requete remvoie pas les resultats mais le nbr de libre infecte
  * 
  * query excecute une requete sql
+ * 
  * */
+//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // exception
+// on peut mettre un 3 eme argument -> set attribut 
+// et on genere une exception
 
-$query = $pdo->query('SELECT * FROM météo');
-if (!($query)) {
-    var_dump($pdo->errorInfo()) ;
-    die('erreur SQL');
-} // si query = false renvoie une erreur
+$error = null; // gere le message d erreur
+try{
 
-/**
- * apres on obtient un pdo statement
- * 
- * excecute lors d une methode prepareee
- * 
- * fetch pour un resultat
- * 
- * fetchAll pour tout les resultats
- * 
- * 
- * fetchColumn pour une colone 
- */
+    if(isset($_POST['ville']) && isset($_POST['haut']) && isset($_POST['bas'])){
+        $query= $pdo->prepare('INSERT INTO météo (ville, haut, bas) VALUES (:ville, :haut, :bas)');
+        $query->execute([
+            'ville' => $_POST['ville'],
+            'haut' => $_POST['haut'],
+            'bas' => $_POST['bas']
+        ]);
+        header('Location: ./edit.php?ville='.$_POST['ville']);
+        // header('Location: ./edit.php?' . $pdo->lastInsertId());
+        exit();
+    }
+    $query = $pdo->query('SELECT * FROM météo');
 
 
-$ville = $query->fetchAll();
-// comme premiere fois on fait un print r
-echo '<pre>';
-print_r($ville);
-echo '</pre>';
+    // if (!($query)) {
+    //     var_dump($pdo->errorInfo()) ;
+    //     die('erreur SQL');    plus oblige car on a mit une exception
+    // } // si query = false renvoie une erreur
 
+    /**
+     * apres on obtient un pdo statement
+     * 
+     * excecute lors d une methode prepareee
+     * 
+     * fetch pour un resultat
+     * 
+     * fetchAll pour tout les resultats
+     * 
+     * 
+     * fetchColumn pour une colone 
+     */
+
+
+    //$ville = $query->fetchAll(PDO::FETCH_OBJ);
+    $ville = $query->fetchAll();
+    /**
+     * on recupere les donne numerica et assoc
+     * 
+     * si on met PDO::FETCH_ASSOC que les assiotiatif
+     * 
+     * PDO::FETCH_BOUND lier les variables au colum(complique)
+     * 
+     * PDO::FETCH_CLASS recup les donnes sous une instance d une class
+     * 
+     * PDO::FETCH_INTO insere les resultat dans une instance deja cree
+     * 
+     * PDO::FETCH_LAZY une combinaise des 2  PDO::FETCH_BOTH and PDO::FETCH_OBJ
+     * 
+     * PDO::FETCH_NAMED retourne un tableau
+     * 
+     * PDO::FETCH_NUM resultat index par un num
+     * 
+     * PDO::FETCH_OBJ sous forme d un obj anonym
+     * 
+     * PDO::FETCH_PROPS_LATE une classe est cree avant
+     */
+
+    // comme premiere fois on fait un print r
+    // echo '<pre>';
+    // print_r($ville[0]->haut);
+    // echo '</pre>';  exemple 
+} catch(PDOException $e){
+    $error = $e->getMessage();
+}
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -48,18 +98,44 @@ echo '</pre>';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 
-<br><form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
-    <label>Ville</label>
-    <input type="text" name="ville" id="ville"> <br>
-    <label>Haut</label>
-    <input type="text" name="haut" id="haut"><br>
-    <label>Bas</label>
-    <input type="text" name="bas" id="bas"><br>
-    <button type="submit" name="submit" id="send">Send</button>
-</form>
+<div class="container">
+    <h2> Ajouter une ville</h2>
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="text" class="from-control" name="ville" value="">
+        </div>
+        <div class="form-group">
+            <input type="text" class="from-control" name="haut" value="">
+        </div>
+        <div class="form-group">
+            <input type="text" class="from-control" name="bas" value="">
+        </div>
+        <button class="btn btn-primary">Send</button>
+    </form>
+
+    <?php if ($error): ?>
+    <p><?= $error ?></p> 
+    <!-- pas le mieux mais bien pour le debug -->
+<?php else: ?>
+    
+        <?php  foreach($ville as $ville): ?> 
+        <h3 class= "my-3"> <a href="./edit.php?ville=<?= $ville->ville ?>"><?= htmlentities($ville->ville) ?></a> </h3>
+        <p> 
+            Max = <?= htmlentities($ville->haut) ?>
+        </p>
+        <p> 
+            Min = <?= htmlentities($ville->bas) ?>
+        </p>
+        <?php endforeach ?>
+    
+
+<?php endif ?>
+</div>
+
 </body>
 </html>
