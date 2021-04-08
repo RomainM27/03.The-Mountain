15d4c627245bad2ea9ccf161fc044b0f74c1a9e7
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-04-08 10:17:58 +0200

    mise en place du mvc d'openclassroom

--- poo/openclassroom/MVC/view/listPostView.php
+++ poo/openclassroom/MVC/view/listPostView.php
@@ -4,19 +4,19 @@
 <head>
     <meta charset="utf-8" />
     <title>Mon blog</title>
-    <link href="style.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 
-<body>
-    <h1>Mon super blog !</h1>
+<body class="container">
+    <h1 class="my-5">Mon super blog !</h1>
     <p>Derniers billets du blog :</p>
 
 
     <?php
     while ($data = $posts->fetch()) {
     ?>
-        <div class="news">
-            <h3>
+        <div class="my-4 card">
+            <h3 class="card-title">
                 <?= htmlspecialchars($data['title']) ?>
                 <em>le <?= $data['creation_date_fr'] ?></em>
             </h3>
@@ -24,7 +24,7 @@
             <p>
                 <?= nl2br(htmlspecialchars($data['content'])) ?>
                 <br />
-                <em><a href="#">Commentaires</a></em>
+                <em><a href="post.php?id=<?= $data['id'] ?>">Commentaires</a></em>
             </p>
         </div>
     <?php
