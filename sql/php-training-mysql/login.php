21fc5eb31712c917a74ab440ff075c5fdbf3db48
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-04-09 14:47:11 +0200

    Update login.php

--- sql/php-training-mysql/login.php
+++ sql/php-training-mysql/login.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 
 require 'pdo.php';
 session_start();
-$error = null; 
+$error = null;
 var_dump($_POST);
 try {
 
 
 
-    
-    if(isset($_POST["button"])) {
+
+    if (isset($_POST["button"])) {
         var_dump($_POST);
-        $login=$_POST['login'];
-        $password=password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $login = $_POST['login'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         var_dump($password);
 
 
-        $sql ="SELECT login, password FROM user WHERE (login=:login)";
-        $query= $pdo -> prepare($sql);
-        $query-> bindParam(':login', $login, PDO::PARAM_STR);
-        $query-> execute();
+        $sql = "SELECT login, password FROM user WHERE (login=:login)";
+        $query = $pdo->prepare($sql);
+        $query->bindParam(':login', $login, PDO::PARAM_STR);
+        $query->execute();
 
 
         // $result = $query->fetch();
@@ -31,14 +31,12 @@ try {
         $result = $query->fetch();
         var_dump($result);
         if (password_verify($_POST["password"], $result->password)) {
-            $_SESSION["login"] = $_POST["login"];
+            $_SESSION["login"] = $login;
             exit(header("location:read.php"));
         } else {
             echo "'Invalid Dessstails'";
-        } 
-    
+        }
     }
-
 } catch (PDOException $e) {
     $error = $e->getMessage();
 }
